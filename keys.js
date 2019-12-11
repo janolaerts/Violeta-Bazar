866634c6d938b898232fc90a60a9cb3599f9a7fd
iamjanolaerts <jan.olaerts92@gmail.com>
2019-12-11 12:17:34 -0500

    Removed keys

---
+++ keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mongodb: {
+    dbURI: 'mongodb+srv://User:PjHWMhZ3DxvNhH3m@products-cd5i4.mongodb.net/test?retryWrites=true&w=majority'
+  },
+  stripe: {
+    publishable: 'pk_test_JqYEkH8szUiNBd5ekTYsTylB00n6FNkQ4K',
+    secret: 'sk_test_Fw5GgWiskJ83qWRT7vdO9U2n00t9OCWHXT'
+  },
+  gmail: {
+    pass: 'Thaliajanet2019'
+  }
+};
