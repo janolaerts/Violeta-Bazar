39c4b2f4d07bfaa88417a5df529c1b053e2c6d3d
iamjanolaerts <jan.olaerts92@gmail.com>
2019-12-11 14:57:29 -0500

    changed production monog string

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongodb: {
-    dbURI: process.env.MONGODB
+    dbURI: `mongodb+srv://${process.env.MongoDBUser}:${process.env.MongoDBPassword}@products-cd5i4.mongodb.net/test?retryWrites=true&w=majority`
   },
   stripe: {
     publishable: process.env.STRIPE_PUBLISHABLE_KEY,
